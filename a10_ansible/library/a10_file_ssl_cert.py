4af997ef41f584da1ee4464c880c884f05dc7c3f
Michael Durrant <mdurrant@a10networks.com>
2018-11-06 20:45:14 +0000

    SSL Certs w/ pattern to be applied to other file objects

---
+++ a10_ansible/library/a10_file_ssl_cert.py
@@ -0,0 +1,319 @@
+#!/usr/bin/python
+
+# Copyright 2018 A10 Networks
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+REQUIRED_NOT_SET = (False, "One of ({}) must be set.")
+REQUIRED_MUTEX = (False, "Only one of ({}) can be set.")
+REQUIRED_VALID = (True, "")
+
+
+DOCUMENTATION = ''' 
+module: a10_file_ssl_cert
+description:
+    - ssl certificate file information and management commands
+short_description: Configures A10 file.ssl-cert
+author: A10 Networks 2018 
+version_added: 2.4
+options:
+    state:
+        description:
+        - State of the object to be created.
+        choices:
+        - present
+        - absent
+        required: True
+    a10_host:
+        description:
+        - Host for AXAPI authentication
+        required: True
+    a10_username:
+        description:
+        - Username for AXAPI authentication
+        required: True
+    a10_password:
+        description:
+        - Password for AXAPI authentication
+        required: True
+    pfx_password:
+        description:
+        - "The password for certificate file (pfx type only)"
+        required: False
+    dst_file:
+        description:
+        - "destination file name for copy and rename action"
+        required: False
+    file:
+        description:
+        - "PRIMARY KEY: Filename used to access certificate in templates"
+        required: False
+    certificate_type:
+        description:
+        - Certificate encoding scheme "'pem'= pem; 'der'= der; 'pfx'= pfx; 'p7b'= p7b; "
+        required: False
+    file_content:
+        description:
+        - SSL Certificate data in PEM/DER/PFX/P7B format 
+'''
+
+EXAMPLES = ''' 
+'''
+
+ANSIBLE_METADATA = {
+    'metadata_version': '1.1',
+    'supported_by': 'community',
+    'status': ['preview']
+}
+
+# Hacky way of having access to object properties for evaluation
+AVAILABLE_PROPERTIES = ["certificate_type","dst_file","file","pfx_password","uuid",]
+
+# our imports go at the top so we fail fast.
+try:
+    from a10_ansible import errors as a10_ex
+    from a10_ansible.axapi_http import client_factory, session_factory
+    from a10_ansible.kwbl import KW_IN, KW_OUT, translate_blacklist as translateBlacklist
+
+except (ImportError) as ex:
+    module.fail_json(msg="Import Error:{0}".format(ex))
+except (Exception) as ex:
+    module.fail_json(msg="General Exception in Ansible module import:{0}".format(ex))
+
+
+def get_default_argspec():
+    return dict(
+        a10_host=dict(type='str', required=True),
+        a10_username=dict(type='str', required=True),
+        a10_password=dict(type='str', required=True, no_log=True),
+        state=dict(type='str', default="present", choices=["present", "absent"])
+    )
+
+def get_argspec():
+    rv = get_default_argspec()
+    rv.update(dict(
+        # pfx_password=dict(type='str',),
+        # dst_file=dict(type='str',),
+        # uuid=dict(type='str',),
+        file=dict(type='str',),
+        certificate_type=dict(type='str',choices=['pem','der','pfx','p7b']),
+        file_content=dict(type='str', no_log=True)
+    ))
+
+    return rv
+
+
+def new_url(module):
+    """Return the URL for creating a resource"""
+    # To create the URL, we need to take the format string and return it with no params
+    url_base = "/axapi/v3/file/ssl-cert"
+    f_dict = {}
+
+    return url_base.format(**f_dict)
+
+
+def existing_url(module):
+    """Return the URL for an existing resource"""
+    # Build the format dictionary
+    url_base = "/axapi/v3/file/ssl-cert/{file}"
+
+    f_dict = {}
+    f_dict["file"] = module.params["file"]
+    return url_base.format(**f_dict)
+
+
+def build_envelope(title, data):
+    return {
+        title: data
+    }
+
+
+def _to_axapi(key):
+    return translateBlacklist(key, KW_OUT).replace("_", "-")
+
+
+def _build_dict_from_param(param):
+    rv = {}
+
+    for k,v in param.items():
+        hk = _to_axapi(k)
+        if isinstance(v, dict):
+            v_dict = _build_dict_from_param(v)
+            rv[hk] = v_dict
+        if isinstance(v, list):
+            nv = [_build_dict_from_param(x) for x in v]
+            rv[hk] = nv
+        else:
+            rv[hk] = v
+
+    return rv
+
+
+def build_json(title, module, action):
+    rv = {}
+    filedata = module.params["file_content"]
+    size = len(filedata)
+    fname = module.params["file"]
+
+    rv = {
+        "file": module.params["file"],
+        "size": size,
+        "file-handle": fname,
+        "certificate-type": module.params["certificate_type"],
+        "action": action
+    }
+
+    return build_envelope(title, rv)
+
+
+def validate(params):
+    # Ensure that params contains all the keys.
+    requires_one_of = sorted([])
+    present_keys = sorted([x for x in requires_one_of if params.get(x)])
+    
+    errors = []
+    marg = []
+
+    action = params.get("action")
+    dst_file = params.get("dst_file")
+    file_content = params.get("file_content")
+
+    if action in ["check", "create", "export", "copy", "rename"]:
+        rc, msg = (False, "{} is not a supported action".format(action))
+
+    if action in ["copy", "rename"]:
+        if not dst_file:
+            rc,msg = (False, "dst_file must be set for {}".format(action))
+
+    if action == "import":
+        if not file_content:
+            rc,msg = (False, "file_content must be specified for import")
+
+    if len(requires_one_of) > 0 and len(present_keys) == 0:
+        rc,msg = REQUIRED_NOT_SET
+        marg = requires_one_of
+    elif requires_one_of and requires_one_of == present_keys:
+        rc,msg = REQUIRED_MUTEX
+        marg = present_keys
+    else:
+        rc,msg = REQUIRED_VALID
+    
+    if not rc:
+        errors.append(msg.format(", ".join(marg)))
+    
+    return rc,errors
+
+def get(module):
+    return module.client.get(existing_url(module))
+
+def exists(module):
+    try:
+        return get(module)
+    except a10_ex.NotFound:
+        return False
+
+def create(module, result):
+    json_payload = build_json("ssl-cert", module, "import")
+    try:
+        file_content=module.params["file_content"]
+
+        module.client.post(new_url(module), json_payload, 
+                           file_content=module.params["file_content"], 
+                           file_name=module.params["file"])
+        # We either get 200 or an error back
+        result["changed"] = True
+    except a10_ex.Exists:
+        result["changed"] = False
+    except a10_ex.ACOSException as ex:
+        module.fail_json(msg=ex.msg, **result)
+    except Exception as gex:
+        raise gex
+    return {} 
+
+def delete(module, result):
+    try:
+        url = "/axapi/v3/pki/delete-oper"
+        payload = {"delete-oper": {"filename": module.params["file"]}}
+        module.client.post(existing_url(module), payload)
+        result["changed"] = True
+    except a10_ex.NotFound:
+        result["changed"] = False
+    except a10_ex.ACOSException as ex:
+        module.fail_json(msg=ex.msg, **result)
+    except Exception as gex:
+        raise gex
+    return result
+
+def update(module, result, existing_config):
+    payload = build_json("ssl-cert", module)
+    changed = False 
+    try:
+        # The only way we get chang
+        module.client.post(existing_url(module), payload)
+        changed = True
+    except a10_ex.ACOSException as ex:
+        changed = False
+        module.fail_json(msg=ex.msg, **result)
+    except Exception as gex:
+        raise gex
+    result["changed"] = changed
+    return result
+
+def present(module, result, existing_config):
+    if not exists(module):
+        return create(module, result)
+    else:
+        return update(module, result, existing_config)
+
+def absent(module, result):
+    return delete(module, result)
+
+def run_command(module):
+    run_errors = []
+
+    result = dict(
+        changed=False,
+        original_message="",
+        message=""
+    )
+
+    state = module.params["state"]
+    a10_host = module.params["a10_host"]
+    a10_username = module.params["a10_username"]
+    a10_password = module.params["a10_password"]
+    # TODO(remove hardcoded port #)
+    a10_port = 443
+    a10_protocol = "https"
+
+    valid = True
+
+    if state == 'present':
+        valid, validation_errors = validate(module.params)
+        map(run_errors.append, validation_errors)
+    
+    if not valid:
+        result["messages"] = "Validation failure"
+        err_msg = "\n".join(run_errors)
+        module.fail_json(msg=err_msg, **result)
+
+    module.client = client_factory(a10_host, a10_port, a10_protocol, a10_username, a10_password)
+    existing_config = exists(module)
+
+    if state == 'present':
+        result = present(module, result, existing_config)
+        module.client.session.close()
+    elif state == 'absent':
+        result = absent(module, result)
+        module.client.session.close()
+    return result
+
+def main():
+    module = AnsibleModule(argument_spec=get_argspec())
+    result = run_command(module)
+    module.exit_json(**result)
+
+# standard ansible module imports
+from ansible.module_utils.basic import *
+from ansible.module_utils.urls import *
+
+if __name__ == '__main__':
+    main()
